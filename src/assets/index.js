c19849e22ccd0cc565c8a5a1b0c03d9ec0143dda
Max de Vocht <maxdevocht@hotmail.com>
2024-08-06 12:09:45 +0200

    Setup Header Component

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import logoIcon from "../../public/favicon.ico";
+import menuIcon from "../assets/icon-menu.svg";
+
+export { logoIcon, menuIcon };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+        lg: "80px",
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1200px",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
